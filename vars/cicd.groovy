5e0146dc92ed84045b00cee3d7adfd2deb6593c0
lalitha419 <97079376+lalitha419@users.noreply.github.com>
2022-01-24 21:21:50 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -8,7 +8,7 @@ def newmaven()
 }
 def newdeploy(ip,appname)
 {
-  sh 'scp /home/ubuntu/.jenkins/workspace/declarativepipeline1/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war'
+  sh "scp /home/ubuntu/.jenkins/workspace/declarativepipeline1/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
 def runselinium(path)
 {
